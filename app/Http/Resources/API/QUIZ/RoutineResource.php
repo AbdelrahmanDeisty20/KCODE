21f8250f1f1d41d7b5c528946e9ee73aa074db0c
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-07-23 10:44:05 +0300

    feat: create RoutineResource to transform routine step and product details for API responses

--- app/Http/Resources/API/QUIZ/RoutineResource.php
+++ app/Http/Resources/API/QUIZ/RoutineResource.php
@@ -37,7 +37,7 @@ class RoutineResource extends JsonResource
         }
 
         return [
-            'routine_id' => $this->routine_id,
+            'id' => $this->routine_id,
             'step_id' => $step ? $step->id : null,
             'step_name' => $step ? ($lang === 'ar' ? $step->name_ar : $step->name_en) : null,
             'step_order' => $this->temp_sequence_order ?? $this->step,
