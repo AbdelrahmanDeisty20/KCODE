bbce6fe1a3766dcc18f4677171867fc963e68e33
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-07-28 13:49:06 +0300

    feat: add base application configuration file

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', (isset($_SERVER['HTTP_HOST']) ? ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https://' : 'http://') . $_SERVER['HTTP_HOST']) : 'http://127.0.0.1:8000')),
 
     /*
     |--------------------------------------------------------------------------
