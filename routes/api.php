eeaac3838440651524437f41735d0838d50c483d
AbdelrahmanDeisty20 <abooda601@gmail.com>
2026-08-11 15:37:55 +0300

    feat: initialize API route definitions for authentication, core resources, and user services

--- routes/api.php
+++ routes/api.php
@@ -205,8 +205,6 @@ Route::middleware([SetLang::class])->group(function () {
         Route::controller(\App\Http\Controllers\API\Genral\AppNotificationController::class)->group(function () {
             Route::get('/user/notifications', 'index');
             Route::post('/user/notifications/read-all', 'markAllAsRead');
-            Route::post('/user/notifications/mark-all-read', 'markAllAsRead');
-            Route::post('/user/notifications/read', 'markAllAsRead');
             Route::post('/user/notifications/{id}/read', 'markAsRead');
         });
 
